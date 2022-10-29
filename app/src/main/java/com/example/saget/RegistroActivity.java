8809aafc5f547eeb2dcd84fde6e6e829a8d996ff
Alonso Rosales <a20191566@pucp.edu.pe>
2022-10-28 23:12:19 -0500

    registro usuario a firebase

--- app/src/main/java/com/example/saget/RegistroActivity.java
+++ app/src/main/java/com/example/saget/RegistroActivity.java
@@ -1,5 +1,6 @@
 package com.example.saget;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,16 +14,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.saget.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class RegistroActivity extends AppCompatActivity {
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        firebaseDatabase = FirebaseDatabase.getInstance("https://saget-d5557-default-rtdb.firebaseio.com/");
 
         List<String> valuesSpinner = new ArrayList<>();
         valuesSpinner.add(0,"Seleccionar");
@@ -42,10 +52,10 @@ public class RegistroActivity extends AppCompatActivity {
         String correo = null;
         String rol = null;
 
-
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
         EditText nombresText = findViewById(R.id.nombresRegistro);
         String nombres = nombresText.getText().toString();
-        if(nombres.equalsIgnoreCase("") || nombres == null){
+        if(nombres.equalsIgnoreCase("") || nombres == null || nombres.isEmpty()){
             nombresText.setError("Ingrese sus nombres");
             guardar = false;
         }
@@ -53,7 +63,7 @@ public class RegistroActivity extends AppCompatActivity {
 
         EditText apellidosText = findViewById(R.id.apellidossRegistro);
         String apellidos = apellidosText.getText().toString();
-        if(apellidos.equalsIgnoreCase("") || apellidos == null){
+        if(apellidos.equalsIgnoreCase("") || apellidos == null || apellidos.isEmpty()){
             apellidosText.setError("Ingrese sus apellidos");
             guardar = false;
         }
@@ -61,26 +71,49 @@ public class RegistroActivity extends AppCompatActivity {
 
         EditText codigoText = findViewById(R.id.codigoPUCP);
         String codigoPUCP = codigoText.getText().toString();
-        if(codigoPUCP.equalsIgnoreCase("") || codigoPUCP == null){
+        if(codigoPUCP.equalsIgnoreCase("") || codigoPUCP == null || codigoPUCP.isEmpty()){
             codigoText.setError("Ingrese su código PUCP");
             guardar = false;
+        }else{
+            try{
+                int code = Integer.parseInt(codigoPUCP);
+                Calendar cal= Calendar.getInstance();
+                int year= cal.get(Calendar.YEAR);
+
+                if(code<1917 || code>year){
+                    codigoText.setError("Ingrese un código PUCP valido");
+                    guardar = false;
+                }
+
+            }catch (NumberFormatException e){
+                codigoText.setError("Ingrese un código PUCP valido");
+                guardar = false;
+            }
+
         }
 
 
         EditText correoText = findViewById(R.id.correoRegistro);
         String correoHelper = correoText.getText().toString();
-        if(correoHelper.equalsIgnoreCase("") || correoHelper == null){
+        if(correoHelper.equalsIgnoreCase("") || correoHelper == null || correoHelper.isEmpty()){
             correoText.setError("Ingrese su correo PUCP");
             guardar = false;
         }else{
             if(correoHelper.contains("@")){
                 String[] partesCorreo = correoHelper.split("@");
-                if (partesCorreo[1].equals("pucp.edu.pe")){
-                    correo = correoHelper;
-                }else{
+
+                if(partesCorreo[0].equals("") || partesCorreo[0].isEmpty()){
                     correoText.setError("Ingrese un correo PUCP valido");
                     guardar = false;
+                }else{
+                    if (partesCorreo[1].equals("pucp.edu.pe")){
+                        correo = correoHelper;
+                    }else{
+                        correoText.setError("Ingrese un correo PUCP valido");
+                        guardar = false;
+                    }
                 }
+
             }else{
                 correoText.setError("Ingrese un correo PUCP valido");
                 guardar = false;
@@ -104,6 +137,7 @@ public class RegistroActivity extends AppCompatActivity {
                     rol = rolHelper;
                 }else{
                     ((TextView)spinner.getSelectedView()).setError("Seleciona un rol válido");
+                    guardar = false;
                 }
             }
         }
@@ -111,18 +145,40 @@ public class RegistroActivity extends AppCompatActivity {
 
         EditText contraText = findViewById(R.id.contrasenaRegistro);
         String contrasena = contraText.getText().toString();
-        if(contrasena.equalsIgnoreCase("") || contrasena == null){
+        if(contrasena.equalsIgnoreCase("") || contrasena == null || contrasena.isEmpty()){
             contraText.setError("Ingrese su contraseña");
             guardar = false;
         }
 
 
+        //GUARDAMOS
         if(guardar){
-            //Guardar y ya
-            Toast.makeText(this, "Cuenta creada exitosamente", Toast.LENGTH_SHORT).show();
+            Usuario usuario = new Usuario(nombres,apellidos,correo,sexo,rol,sha256(contrasena));
+            databaseReference.child("usuario").child(codigoPUCP).setValue(usuario);
+            Toast.makeText(this,"Cuenta creada exitosamente",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Campo(s) incorrecto(s)",Toast.LENGTH_SHORT).show();
         }
 
     }
 
+    public String sha256(String base) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch(Exception ex){
+
+            return base;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/saget/Usuario.java
@@ -0,0 +1,67 @@
+package com.example.saget;
+
+public class Usuario {
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String sexo;
+    private String rol;
+    private String password;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Usuario(String nombres, String apellidos, String correo, String sexo, String rol, String password) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.sexo = sexo;
+        this.rol = rol;
+        this.password = password;
+    }
+}
