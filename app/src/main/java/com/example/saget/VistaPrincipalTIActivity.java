3f3ccc59714fe66abaf983185636be1b8d92dfd6
Joaquin Orellana <joaquin.orellana@pucp.edu.pe>
2022-11-25 00:04:04 -0500

    Avance Vistas ti

---
+++ app/src/main/java/com/example/saget/VistaPrincipalTIActivity.java
@@ -0,0 +1,40 @@
+package com.example.saget;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class VistaPrincipalTIActivity extends AppCompatActivity {
+
+    Fragment fragmentInicioTI = new InicioFragmentTi();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_principal_tiactivity);
+        BottomNavigationView bottomNavigationViewTI = findViewById(R.id.bottom_navigation_ti);
+        bottomNavigationViewTI.setOnItemSelectedListener(item ->{
+            switch(item.getItemId()){
+             case R.id.house_ti:
+                 loadFragment(fragmentInicioTI);
+                 return true;
+             default:
+                return false;
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container_TI,fragment);
+        transaction.commit();
+    }
+
+}
